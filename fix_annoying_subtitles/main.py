7feb93b1d507be0b8ae4b1c6a8f2ec81b1ac02dd
v4mp <iqbalmuchlis768@gmail.com>
2022-12-01 14:06:19 +0700

    merged the functions

--- fix_annoying_subtitles/main.py
+++ fix_annoying_subtitles/main.py
@@ -10,17 +10,32 @@ import re
 
 TEXT = r'Subtitle_1.srt'
 
-def no_dash():
+# def no_dash():
+#     eng_sub = open(TEXT).read()
+#     no_dash_eng_sub = re.sub(r'-\(.*\)', '', eng_sub)
+
+#     open(TEXT, "w").write(no_dash_eng_sub)
+
+# def no_braces():
+#     eng_sub = open(TEXT).read()
+#     no_braces_eng_sub = re.sub(r'\(.*\)', '', eng_sub)
+
+#     open(TEXT, "w").write(no_braces_eng_sub)
+
+def no_dash_and_braces():
+    #dash
     eng_sub = open(TEXT).read()
     no_dash_eng_sub = re.sub(r'-\(.*\)', '', eng_sub)
 
     open(TEXT, "w").write(no_dash_eng_sub)
 
-def no_braces():
+    #braces
     eng_sub = open(TEXT).read()
     no_braces_eng_sub = re.sub(r'\(.*\)', '', eng_sub)
 
     open(TEXT, "w").write(no_braces_eng_sub)
 
-no_dash()
-no_braces()
+
+# no_dash()
+# no_braces()
+no_dash_and_braces()
