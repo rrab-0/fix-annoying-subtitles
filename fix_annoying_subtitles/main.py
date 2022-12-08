14e22039541a4f2252c693ac9b5bf688d9220dd1
v4mp <iqbalmuchlis768@gmail.com>
2022-12-08 20:07:13 +0700

    checkpoint for testing

--- fix_annoying_subtitles/main.py
+++ fix_annoying_subtitles/main.py
@@ -2,40 +2,21 @@
 #then deletes them and characters inside them too
 # !!didnt work this time idk what happened!!
 
-
-# update, it works now
-# no need to use findall, sub function does the work itself
-
 import re
 
 TEXT = r'Subtitle_1_2.srt'
 
-# def no_dash():
-#     eng_sub = open(TEXT).read()
-#     no_dash_eng_sub = re.sub(r'-\(.*\)', '', eng_sub)
-
-#     open(TEXT, "w").write(no_dash_eng_sub)
-
-# def no_braces():
-#     eng_sub = open(TEXT).read()
-#     no_braces_eng_sub = re.sub(r'\(.*\)', '', eng_sub)
-
-#     open(TEXT, "w").write(no_braces_eng_sub)
-
-def no_dash_and_braces():
-    #dash
-    eng_sub = open(TEXT).read()
+def no_dash_and_braces(input):
+    input = input()
+    eng_sub = open(input).read()
     no_dash_eng_sub = re.sub(r'-\(.*\)', '', eng_sub)
 
-    open(TEXT, "w").write(no_dash_eng_sub)
+    open(input, "w").write(no_dash_eng_sub)
 
     #braces
     eng_sub = open(TEXT).read()
     no_braces_eng_sub = re.sub(r'\(.*\)', '', eng_sub)
 
-    open(TEXT, "w").write(no_braces_eng_sub)
+    open(input, "w").write(no_braces_eng_sub)
 
-
-# no_dash()
-# no_braces()
 no_dash_and_braces()
