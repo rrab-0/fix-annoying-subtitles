52556446fb5d35c6187f4a782cf6197258327448
v4mp <iqbalmuchlis768@gmail.com>
2023-01-18 13:12:43 +0700

    formatting

--- fix_annoying_subtitles/main.py
+++ fix_annoying_subtitles/main.py
@@ -1,22 +1,24 @@
-#finds if there's (TEXTexample) and -(TEXTexample)
-#then deletes them and characters inside them too
+# finds if there's (TEXTexample) and -(TEXTexample)
+# then deletes them and characters inside them too
 # !!didnt work this time idk what happened!!
 
 import re
 
 file_name = input()
 
+
 def no_dash_and_braces():
-    
+
     eng_sub = open(file_name).read()
     no_dash_eng_sub = re.sub(r'-\(.*\)', '', eng_sub)
 
     open(file_name, "w").write(no_dash_eng_sub)
 
-    #braces
+    # braces
     eng_sub = open(file_name).read()
     no_braces_eng_sub = re.sub(r'\(.*\)', '', eng_sub)
 
     open(file_name, "w").write(no_braces_eng_sub)
+
 
 no_dash_and_braces()
